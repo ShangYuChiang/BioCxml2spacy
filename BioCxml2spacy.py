a23e6903d6a3e2d73cb11f49635adca20e342de7
ShangYuChiang <shelly200318@hotmail.com.tw>
2020-05-08 13:35:13 -0400

    Updated convert BioCxml into spacy(one file)

--- BioCxml2spacy.py
+++ BioCxml2spacy.py
@@ -3,17 +3,18 @@ import xml.etree.cElementTree as ET
 def xml2spacy(tree):
 
         root = tree.getroot()
-        training_data = ()
+        training_data = []
         spacy_text=""
         #print(len(passages)) #result should be 2
         passages = root.find('document').findall('passage') 
 
         #convert and combine all passage.text(title, abstract) into spacy text format
-        spacy_text += (passages[0].find('text').text + " ")
-        spacy_text += (passages[1].find('text').text)
-        print(spacy_text)
+        spacy_text += (passages[0].find("text").text + " ")
+        spacy_text += (passages[1].find("text").text)
         training_data.append(spacy_text)
-        spacy_entities= annotation2entity(passages)
+        training_data.append(annotation2entity(passages))
+        training_data = tuple(training_data)
+        return training_data
 
 def annotation2entity(passages):
         entities = ", {\"entities\":"
@@ -21,23 +22,22 @@ def annotation2entity(passages):
                 passage_annotations = passage.findall('annotation')
                 #print(len(passage_annotations))
                 
-        
-        '''
-        passage_offset = int(passage.find('offset').text)
-        passage_text = passage.find('text').text
-        passage_entities = []
-        for ann in passage_annotations:
-                entity_type = ann.find('./infon[@key="type"]').text
-                od = ann.find('./location').attrib
-                start, end = get_entity_offset(od, passage_offset)
-                passage_entities.append((start, end, entity_type))
-
-                spacyd_passage = (passage_text, {"entities": passage_entities})
-                spacy_data.append(spacyd_package)
-        '''
+                passage_entities = []
+                for ann in passage_annotations:
+                        entity_type = ann.find('./infon[@key="type"]').text
+                        #print(entity_type)
+                        if entity_type == 'Gene':
+                                od = ann.find('./location').attrib
+                                start = int(od['offset'])
+                                end = start + int(od['length'])
+                                passage_entities.append((start, end, entity_type))
+        spacyd_passage = {"entities": passage_entities}
+        return spacyd_passage
+                
         
 if __name__ == "__main__":
         tree = ET.parse('./input/10835480_v0.xml')
         xml2spacy(tree)
         spacy_data = []
+        spacy_data.append(xml2spacy(tree))
         print(spacy_data)
